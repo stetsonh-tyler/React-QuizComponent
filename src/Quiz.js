7c941321196b1d5fb69918a04efd9e1161a5d887
hannah-stetson <hannah.stetson@maine.edu>
2020-11-12 14:55:44 -0500

    module4

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,7 @@
 import { render } from 'enzyme'
 import React, { Component } from 'react'
 import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd.js'
 
 let quizData = require('./quiz_data.json')
 
@@ -12,7 +13,16 @@ class Quiz extends Component {
     }
 
     render() {
-        return (<div><QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/></div>)
+        const isQuizEnd = this.state.quiz_position - 1 == quizData.quiz_questions.length
+
+        if (!isQuizEnd) {
+            return (
+                <div>
+                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+                </div>)
+        } else {
+            return (<div><QuizEnd /></div>)
+        }
     }
 }
 
