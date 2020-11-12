a8616c91d5a592cfee69f8cace16eab62c475a7a
hannah-stetson <hannah.stetson@maine.edu>
2020-11-12 14:32:26 -0500

    module2

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,23 +1,19 @@
-import { render } from 'enzyme';
-import React, { Component } from 'react';
+import { render } from 'enzyme'
+import React, { Component } from 'react'
+import QuizQuestion from './QuizQuestion.js'
 
-let quizData = require('./quiz_data.json');
+let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
 
     constructor (props) {
-        super(props);
-        this.state = {quiz_position: 1};
+        super(props)
+        this.state = {quiz_position: 1}
     }
 
     render() {
-        return (
-        <div>
-            <div className="QuizQuestion">
-                {quizData.quiz_questions[0].instruction_text}
-            </div>
-        </div>);
+        return (<div><QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/></div>)
     }
 }
 
-export default Quiz;
+export default Quiz
